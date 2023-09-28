f42727107bfbf985d84fe820257f19e7f7fe178e
unknown <pradyumnyadu02@gmail.com>
2023-09-29 01:26:27 +0530

    adding all of the js files including the new js rockPaperScissors game.

---
+++ functons.js
@@ -0,0 +1,18 @@
+function addTwoNum(num1,num2){
+    console.log(num1+num2)
+}
+
+addTwoNum(10,10)
+
+// functions as expression
+var addTwo = function(a,b){
+    return a+b
+}
+
+console.log(addTwo(1,5))
+
+
+num1 = 5
+num2 = 6
+
+console.log(num1*num2)
