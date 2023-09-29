7684a553e6d2af5be30ccc19940a66211b7db36f
unknown <pradyumnyadu02@gmail.com>
2023-09-29 22:30:02 +0530

    adding modularity files.

---
+++ importModularity.js
@@ -0,0 +1,9 @@
+
+// importing module.
+const calculatorModule = require("./modularity.js")
+
+// using module's functions.
+calculatorModule.addition(4,5)
+calculatorModule.substraction(4,5)
+calculatorModule.multiplication(4,5)
+calculatorModule.division(4,5)
