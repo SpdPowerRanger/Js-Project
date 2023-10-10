c18bfa29b7d7a012660ed36f73f0a30728cf9b50
unknown <pradyumnyadu02@gmail.com>
2023-10-10 21:04:31 +0530

    Constructor function in js.

---
+++ constructorFn.js
@@ -0,0 +1,17 @@
+
+// this is a constructor function.
+function createCar (name,company,color){
+    this.name = name,
+    this.company = company,
+    this.color = color
+
+    this.drive = ()=>{  // method inside a constructor function.
+        console.log(`I drive a ${this.color} ${this.name}. I love my ${this.name}.`) 
+    }
+}
+
+let car1 = new createCar('Cresta',"Toyota","White")
+
+console.log(car1)
+
+car1.drive()
